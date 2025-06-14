f858eb6dbe42b9a76fdd03932c3bbb54d4fd5f16
shankkarpal46 <shankkarpal46@gmail.com>
2025-06-14 14:53:00 +0530

    components and props

---
+++ intro/src/component/Button.jsx
@@ -0,0 +1,15 @@
+function Button({data}){
+    // return(
+    //     <>
+    //         <button>Button Component {props.data}</button>
+    //     </>
+    // ) 
+
+    return(
+        <>
+            <button>Button Component.{data}</button>
+        </>
+    )
+}
+
+export default Button
---
+++ intro/src/component/My.jsx
@@ -0,0 +1,5 @@
+function My(){
+    return <h1>This is my component.</h1>
+}
+
+export default My
